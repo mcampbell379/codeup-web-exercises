b500e0e3e24e597e21d3194fba8908f3a6f7d98a
Emi Campbell <emicampbell@Emis-MacBook-Pro.local>
2022-11-21 11:05:12 -0600

    comparing operators

---
+++ playground.js
@@ -0,0 +1,8 @@
+//  sandbox for testing js code
+
+// == only holds value, while === includes typeOf
+console.log("'3' == 3 is " + ("3" == 3))
+console.log("'3' === 3 is " + ("3" === 3))
+console.log("'3' !== 3 is " + ("3" !== 3))
+console.log("'3' != 3 is " + ("3" != 3))
+
