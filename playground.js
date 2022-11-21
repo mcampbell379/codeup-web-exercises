f09e55f497eb477c3e1b5b250c6b56513f9c214c
Emi Campbell <emicampbell@Emis-MacBook-Pro.local>
2022-11-21 11:43:44 -0600

    adding variables and block scope

--- playground.js
+++ playground.js
@@ -1,8 +1,17 @@
 //  sandbox for testing js code
 
 // == only holds value, while === includes typeOf
-console.log("'3' == 3 is " + ("3" == 3))
+console.log(`'3' == 3 is ${"3" === 3}`)
 console.log("'3' === 3 is " + ("3" === 3))
 console.log("'3' !== 3 is " + ("3" !== 3))
-console.log("'3' != 3 is " + ("3" != 3))
+console.log(`'3' != 3 is ${"3" !== 3}`)
+
+//variables and block scope
+
+var num = 1;
+{
+    var num = 2;
+        console.log(num)
+}
+console.log(num);
 
