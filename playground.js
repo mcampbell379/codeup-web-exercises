8e1fe2232afce1bcf60308c956b30c1e04c8821d
Emi Campbell <emicampbell@Emis-MacBook-Pro.local>
2022-11-21 11:57:52 -0600

    fixed variable section

--- playground.js
+++ playground.js
@@ -6,12 +6,33 @@ console.log("'3' === 3 is " + ("3" === 3))
 console.log("'3' !== 3 is " + ("3" !== 3))
 console.log(`'3' != 3 is ${"3" !== 3}`)
 
-//variables and block scope
+//var vs let and block scope
 
-var num = 1;
+var varNum = 1;
 {
-    var num = 2;
-        console.log(num)
+    var varNum = 2;
+        console.log(varNum)
 }
-console.log(num);
+console.log(varNum);
 
+let letNum = 1;
+{
+    let  letNum = 2;
+    console.log(letNum)
+}
+console.log(letNum);
+
+
+/*should come out as an error (false statement)
+if (true){
+    let isolatedVar = 69;
+}
+console.log(isolatedVar)
+*/
+
+
+//should come out as "69"
+if (true){
+    var normalVar = 69;
+}
+console.log(normalVar)
