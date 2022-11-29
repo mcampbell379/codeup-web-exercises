4c7f729efc418b2b53e738aa112326d3e092ad22
mcampbell379 <campbell.mitchell45@gmail.com>
2022-11-29 10:30:05 -0600

    adding blackjack

---
+++ blackjack/js/input.js
@@ -0,0 +1,15 @@
+(function () {
+    "use strict";
+
+
+function mainMenu (){
+    alert(`Player score: ${playerScore}\tComp Score: ${compScore}`);
+    alert(`1. Play Blackjack\n2.Exit Game`);
+    let choice = prompt(`Choose your selection: `);
+
+    return choice;
+}
+
+
+
+})();
---
+++ blackjack/js/main.js
@@ -0,0 +1,17 @@
+(function (){
+    "use strict";
+
+
+
+    let playing = false;
+    let playerScore = 0;
+    let compScore = 0;
+
+    while(playing === false){
+
+
+
+    }
+
+
+})();
