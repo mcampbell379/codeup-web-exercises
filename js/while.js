1e7705a4a4fdd298b8499a2265e3c632f1a33850
mcampbell379 <campbell.mitchell45@gmail.com>
2022-12-01 09:28:13 -0600

    added notes to loops

--- js/while.js
+++ js/while.js
@@ -6,18 +6,22 @@ while(num < 65536){
     console.log(num);
 }
 
+//establishes number of cones available to sell
 let coneStock = Math.floor(Math.random() * (100 - 50) + 50);
 
 do {
+    //determines the number of cones bought by individual customer
     let conesBought = Math.floor(Math.random() * 5) + 1;
     if(conesBought <= coneStock) {
         console.log(`Customer bought ${conesBought} cones.`);
         coneStock -= conesBought;
         console.log(`Cones left: ${coneStock}`);
+        //when stock is empty
         if(coneStock === 0){
             console.log(`Yay I sold them all!`);
         }
     } else {
+        //cannot sell more cones than whats left in stock
         console.log(`Cannot sell ${conesBought} I only have ${coneStock} left.`);
     }
-} while(coneStock > 0)
+} while(coneStock > 0);
