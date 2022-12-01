a2511a8086431080da490291cb8b7aee627c61a2
mcampbell379 <campbell.mitchell45@gmail.com>
2022-12-01 12:47:50 -0600

    updated iterating.js

--- js/iterating.js
+++ js/iterating.js
@@ -6,7 +6,7 @@
      * Create an array of 4 people's names and store it in a variable called
      * 'names'.
      */
-    let names = [`Christine`, `Angie`, `Billie`, `Micah`];
+    let names = [`Christine`, `Angie`, `Billie`, `Harvest`];
     /**
      * TODO:
      * Create a log statement that will log the number of elements in the names
