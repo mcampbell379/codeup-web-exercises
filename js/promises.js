20a5dbfceed777b6036a81ab6cdd69cf3b3a34f8
mcampbell379 <campbell.mitchell45@gmail.com>
2023-01-30 15:44:19 -0600

    updated promises.js/.html

--- js/promises.js
+++ js/promises.js
@@ -1,24 +1,32 @@
 "use strict";
+(async function () {
+    function lastCommit(username, apikey) {
+        const url = `https://api.github.com/users/${username}/events/public`
+        return fetch(url, {headers: {'Authorization': `token ${apikey}`}})
+            .then((response) => response.json())
+            .then(data => {
+                return data.filter(function (event){
+                    return event.type === "PushEvent";
+                })[0].created_at;
+            });
+    }
 
-function lastCommit(username,reponame){
-    fetch(`https://api.github.com/repos/${username}/${reponame}/commits`,{headers: {'Authorization': GITHUB_API_KEY}})
+    const lastCommitDate = await lastCommit('mcampbell379', GITHUB_API_KEY)
 
-        .then((response) => response.json())
-        .then((data) => console.log(data[0].commit.author.date))
-        .catch((error) => {
-            console.log('ERROR:' + error);
-    });
-}
-lastCommit('mcampbell379','codeup-web-exercises');
+    console.log(lastCommitDate);
 
 
+////////////////////////////////////////////////////////////////////////
 
-function wait(millisec){
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve();
-        },millisec)
-    });
-}
-wait(1000).then(() => console.log("You\'ll see this after 1 second"));
-wait(3000).then(() => console.log("You\'ll see this after 3 seconds"));
+    function wait(millisec) {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve();
+            }, millisec);
+        });
+    }
+
+    wait(1000).then(() => console.log("You\'ll see this after 1 second"));
+    wait(3000).then(() => console.log("You\'ll see this after 3 seconds"));
+
+})();
